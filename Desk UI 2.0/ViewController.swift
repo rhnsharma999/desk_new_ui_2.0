db93eec258244bba6351c129941d495468a8eb5e
rhnsharma999 <rhnsharma999@gmail.com>
2017-08-27 22:23:02 +0530

    Initial Commit

---
+++ Desk UI 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desk UI 2.0
+//
+//  Created by Rohan Lokesh Sharma on 27/08/17.
+//  Copyright © 2017 webarch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
